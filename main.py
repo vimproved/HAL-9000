215c4998a160f302bd23539ee24d9b7fbb8477bb
joshop <joshop4646@gmail.com>
2020-06-04 19:52:45 -0400

    Added some useful stuff

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
-import NAME as cmds_NAME
+import legacy as cmds_legacy
 import discord
 
 client = discord.Client()
+prefix = "//"
 
 @client.event
 async def on_ready():
-    print('We have logged in as {0.user}'.format(client))
-    
-client.run('NzE3MDQyMTI2Nzc2NDM0NzI4.XtmFVQ.X19EEdNIgbKuYpy5frWM5cXQlHg')
+	print('We have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+	if message.author != client.user and message.content.startswith(prefix):
+		text = message.content[:len(prefix)]
+		run_command(text.split()[0], ' '.join(text.split()[1:]))
+
+def run_command(name, arguments):
+	for g in globals():
+		m = globals()[g]
+		if g.startswith("cmds_") and type(m) == type(__builtins__):
+			for c in m.cmds:
+				if c == arg:
+					m.cmds[c][1](arguments, client)
+
+client.run(open("token").read())
