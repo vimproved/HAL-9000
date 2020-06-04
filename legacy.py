ce6c320986247abbfb365df6103c5de269709c81
joshop <joshop4646@gmail.com>
2020-06-04 19:11:51 -0400

    Legacy file made

---
+++ legacy.py
@@ -0,0 +1,11 @@
+
+cmds = {"time": ("Displays the time.", display_time),
+	"flip": ("Flips a coin.", flip_coin)}
+# To get descriptions: cmds[command][0]
+# To run: cmds[command][1](arguments_as_a_string, bot_object)
+def display_time(args, bot):
+	"""Command for displaying time. Ignores arguments."""
+	pass # unimplemented
+def flip_coin(args, bot):
+	"""Command for flipping a coin. Ignores arguments."""
+	pass # also, unimplemented
