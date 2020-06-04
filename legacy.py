05f8600428d45d5d40e28868a92597851616d5de
joshop <joshop4646@gmail.com>
2020-06-04 19:56:37 -0400

    Ping command works in theory

--- legacy.py
+++ legacy.py
@@ -1,11 +1,15 @@
 
 cmds = {"time": ("Displays the time.", display_time),
-	"flip": ("Flips a coin.", flip_coin)}
+	"flip": ("Flips a coin.", flip_coin),
+	"ping": ("Responds with a pong message.", ping_msg)}
 # To get descriptions: cmds[command][0]
-# To run: cmds[command][1](arguments_as_a_string, bot_object)
-def display_time(args, bot):
+# To run: cmds[command][1](arguments_as_a_string, bot_object, requested_channel)
+async def display_time(args, bot, channel):
 	"""Command for displaying time. Ignores arguments."""
 	pass # unimplemented
-def flip_coin(args, bot):
+async def flip_coin(args, bot, channel):
 	"""Command for flipping a coin. Ignores arguments."""
 	pass # also, unimplemented
+async def ping_msg(args, bot, channel):
+	"""Pings the bot. Ignores arguments."""
+	await channel.send("Pong!")
--- main.py
+++ main.py
@@ -12,14 +12,14 @@ async def on_ready():
 async def on_message(message):
 	if message.author != client.user and message.content.startswith(prefix):
 		text = message.content[:len(prefix)]
-		run_command(text.split()[0], ' '.join(text.split()[1:]))
+		await run_command(text.split()[0], ' '.join(text.split()[1:]), message.channel)
 
-def run_command(name, arguments):
+async def run_command(name, arguments, channel):
 	for g in globals():
 		m = globals()[g]
 		if g.startswith("cmds_") and type(m) == type(__builtins__):
 			for c in m.cmds:
 				if c == arg:
-					m.cmds[c][1](arguments, client)
+					await m.cmds[c][1](arguments, client, channel)
 
 client.run(open("token").read())
