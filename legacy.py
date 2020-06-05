c8b50c6e1c9cf264377c3b79f0bfeac64126307e
joshop <joshop4646@gmail.com>
2020-06-04 20:25:54 -0400

    Fixed another dumb error

--- legacy.py
+++ legacy.py
@@ -1,7 +1,3 @@
-
-cmds = {"time": ("Displays the time.", display_time),
-	"flip": ("Flips a coin.", flip_coin),
-	"ping": ("Responds with a pong message.", ping_msg)}
 # To get descriptions: cmds[command][0]
 # To run: cmds[command][1](arguments_as_a_string, bot_object, requested_channel)
 async def display_time(args, bot, channel):
@@ -13,3 +9,6 @@ async def flip_coin(args, bot, channel):
 async def ping_msg(args, bot, channel):
 	"""Pings the bot. Ignores arguments."""
 	await channel.send("Pong!")
+cmds = {"time": ("Displays the time.", display_time),
+	"flip": ("Flips a coin.", flip_coin),
+	"ping": ("Responds with a pong message.", ping_msg)}
