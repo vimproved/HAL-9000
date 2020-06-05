ca275dc6c1e2493cefc0947bf22f895c55259f7b
joshop <joshop4646@gmail.com>
2020-06-04 22:15:33 -0400

    Fixed a dumb syntax error and added global help

--- main.py
+++ main.py
@@ -13,16 +13,24 @@ async def on_ready():
 async def on_message(message):
 	if message.author != client.user and message.content.startswith(prefix):
 		text = message.content[len(prefix):]
+		if text == "help":
+			s = "Command sets: "
+			for g in globals():
+				m = globals()[g]
+				if g.startswith("cmds_") and type(m) == type(__builtins__):
+					s += "`%s`: %s" % (g[5:], m.desc)
+			await channel.send(s)
 		try:
 			await run_command(text.split()[0], ' '.join(text.split()[1:]), message.channel)
-		except:
+		except Exception as e:
+			print(e)
 			await message.channel.send("I'm sorry, <@%d>, I'm afraid I can't do that." % message.author.id)
 async def run_command(name, arguments, channel):
 	for g in globals():
 		m = globals()[g]
 		if g.startswith("cmds_") and type(m) == type(__builtins__):
 			if name == "help" and arguments == g[5:]:
-				await channel.send("`%s`: %s" % (g[5:], g.desc))
+				await channel.send("`%s`: %s" % (g[5:], m.desc))
 				return
 			for c in m.cmds:
 				if name == "help" and arguments.split()[0] == g[5:] and arguments.split()[1] == c:
