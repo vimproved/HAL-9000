1a0956f88d20863b509711682d2dcde250137152
joshop <joshop4646@gmail.com>
2020-06-05 14:08:31 -0400

    Removed dnd test command and added critical success/failure messages.

--- dnd.py
+++ dnd.py
@@ -1,7 +1,5 @@
 import requests
 from fuzzywuzzy import process
-async def dnd_test(arguments, bot, channel):
-	await channel.send("HORSE")
 async def dnd_get(arguments, bot, channel):
 	endpoint, *args = arguments.split()
 	name = ' '.join(args[:args.index('/')])
@@ -18,6 +16,5 @@ async def dnd_get(arguments, bot, channel):
 			await channel.send("`%s`: *not defined*" % val)
 		else:
 			await channel.send("`%s => %s`" % (real_val, str(data[real_val])))
-cmds = {"dnd-test": ("test dnd command", dnd_test),
-	"dnd-get": ("Gets data from the D&D5e API.\nUsage: `dnd-get <endpoint type> <name (can be more than one word)> / <types of data you want to get separated by spaces>`\nAvailable endpoint types:\n`ability-scores`, `classes`, `conditions`, `damage-types`, `equipment-categories`, `equipment`, `features`, `languages`, `magic-schools`, `monsters`, `proficiencies`, `races`, `skills`, `spellcasting`, `spells`, `starting-equipment`, `subclasses`, `subraces`, `traits`, `weapon-properties`.\nExample: `dnd-get spells Acid Arrow / level`\nUses a fuzzy search algorithm to find the object/property name closest to what you typed.", dnd_get)}
+cmds = {"dnd-get": ("Gets data from the D&D5e API.\nUsage: `dnd-get <endpoint type> <name (can be more than one word)> / <types of data you want to get separated by spaces>`\nAvailable endpoint types:\n`ability-scores`, `classes`, `conditions`, `damage-types`, `equipment-categories`, `equipment`, `features`, `languages`, `magic-schools`, `monsters`, `proficiencies`, `races`, `skills`, `spellcasting`, `spells`, `starting-equipment`, `subclasses`, `subraces`, `traits`, `weapon-properties`.\nExample: `dnd-get spells Acid Arrow / level`\nUses a fuzzy search algorithm to find the object/property name closest to what you typed.", dnd_get)}
 desc = "Commands to assist with D&D."
--- rngesus.py
+++ rngesus.py
@@ -1,6 +1,8 @@
 import random
 async def roll_dice(arguments, bot, channel):
 	total = 0
+	crits = 0
+	critf = 0
 	for die in arguments.split():
 		if 'd' not in die:
 			total += int(die)
@@ -8,7 +10,11 @@ async def roll_dice(arguments, bot, channel):
 		num, sides = die.split('d')
 		if num == '':
 			num = '1'
-		total += sum([random.randint(1, int(sides)) for n in range(int(num))])
-	await channel.send("Result: "+str(total))
+		rolls = [random.randint(1, int(sides)) for n in range(int(num))]
+		if sides == '20':
+			crits += rolls.count(20)
+			critf += rolls.count(1)
+		total += sum(rolls)
+	await channel.send("Result: "+str(total)+"\n***CRITICAL SUCCESS!***"*crits+"\n***CRITICAL FAILURE!***"*critf)
 cmds = {"roll":("Rolls dice.\nSeparate dice to add up with spaces.\nThese are all valid dice: 'd6' '1d6' '5'", roll_dice)}
 desc = "An advanced dice roller, with a basic placeholder for now."
