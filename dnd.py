160876c3807a94ca628d640d35ec5e4e17b21541
joshop <joshop4646@gmail.com>
2020-06-05 12:14:54 -0400

    Fixed a one-letter typo in the help message and changed output format for dnd.py

--- dnd.py
+++ dnd.py
@@ -17,7 +17,7 @@ async def dnd_get(arguments, bot, channel):
 		if not real_val: 
 			await channel.send("`%s`: *not defined*" % val)
 		else:
-			await channel.send("`%s`: %s" % (real_val, str(data[real_val])))
+			await channel.send("`%s => %s`" % (real_val, str(data[real_val])))
 cmds = {"dnd-test": ("test dnd command", dnd_test),
-	"dnd-get": ("Gets data from the D&D5e API.\nUsage: `dnd-get <endpoint type> <name (can be more than one word)> / <types of data you want to get separated by spaces>`\nAvailable endpoint types:\n`ability-scores`, `classes`, `conditions`, `damage-types`, `equipment-categories`, `equipment`, `features`, `languages`, `magic-schools`, `monsters`, `proficiencies`, `races`, `skills`, `spellcasting`, `spells`, `starting-equipment`, `subclasses`, `subraces`, `traits`, `weapon-properties`.\nExample: `dnd-get spell Acid Arrow / level`\nUses a fuzzy search algorithm to find the object/property name closest to what you typed.", dnd_get)}
+	"dnd-get": ("Gets data from the D&D5e API.\nUsage: `dnd-get <endpoint type> <name (can be more than one word)> / <types of data you want to get separated by spaces>`\nAvailable endpoint types:\n`ability-scores`, `classes`, `conditions`, `damage-types`, `equipment-categories`, `equipment`, `features`, `languages`, `magic-schools`, `monsters`, `proficiencies`, `races`, `skills`, `spellcasting`, `spells`, `starting-equipment`, `subclasses`, `subraces`, `traits`, `weapon-properties`.\nExample: `dnd-get spells Acid Arrow / level`\nUses a fuzzy search algorithm to find the object/property name closest to what you typed.", dnd_get)}
 desc = "Commands to assist with D&D."
