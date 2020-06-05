e51ad6e0a3aca211973badcbb83b32ede98ce25e
joshop <joshop4646@gmail.com>
2020-06-04 20:36:38 -0400

    Added time and flip

--- legacy.py
+++ legacy.py
@@ -1,11 +1,13 @@
+from datetime import datetime
+import random
 # To get descriptions: cmds[command][0]
 # To run: cmds[command][1](arguments_as_a_string, bot_object, requested_channel)
 async def display_time(args, bot, channel):
 	"""Command for displaying time. Ignores arguments."""
-	pass # unimplemented
+	await channel.send(datetime.now().strftime("%H:%M:%S on %a, %B %d, %Y"))
 async def flip_coin(args, bot, channel):
 	"""Command for flipping a coin. Ignores arguments."""
-	pass # also, unimplemented
+	await channel.send(random.choice(["Heads!"*50, "Tails!"*50, "The coin landed on the side!!"]))
 async def ping_msg(args, bot, channel):
 	"""Pings the bot. Ignores arguments."""
 	await channel.send("Pong!")
