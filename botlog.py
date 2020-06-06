5ae7f40cc36a73686657bcbb3a21690ad8dde690
Paradigmmmm <retroturtlerocks@gmail.com>
2020-06-05 22:28:39 -0400

    Pickle config system added.

--- botlog.py
+++ botlog.py
@@ -1,4 +1,9 @@
 import pickle
 async def botlog(subcmd):
     if subcmd=="config":
+        msgauth1 = message.author
         message.channel.send("What channel would you like log messages to be posted in?")
+        botlogchannel = await channel.history().find(lambda m: m.author.id == msgauth1)
+        guildchannellist = pickle.load( open( "guildchannellist", "rb" ))
+        guildchannellist.update({message.guild : botlogchannel})
+        pickle.dump(guildchannellist, open("guildchannellist", "wb"))
