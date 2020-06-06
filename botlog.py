e77d18e8be7e9fdd35ee87120c0fff1e74f0fefb
Paradigmmmm <retroturtlerocks@gmail.com>
2020-06-05 22:44:41 -0400

    boof

--- botlog.py
+++ botlog.py
@@ -1,8 +1,8 @@
 import pickle
-async def botlog(subcmd):
-    if subcmd=="channel":
+async def botlog(args, bot, channel):
+    if args=="channel":
         msgauth1 = message.author
-        message.channel.send("What channel would you like log messages to be posted in?")
+        await channel.send("What channel would you like log messages to be posted in?")
         botlogchannel = await channel.history().find(lambda m: m.author.id == msgauth1)
         guildchannellist = pickle.load( open( "guildchannellist", "rb" ))
         guildchannellist.update({message.guild : botlogchannel})
