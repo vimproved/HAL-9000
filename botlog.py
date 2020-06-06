68ebf62f8ffc911d5eedc4281f955d70e45ad41a
Paradigmmmm <retroturtlerocks@gmail.com>
2020-06-05 22:06:00 -0400

    Created File.

--- botlog.py
+++ botlog.py
@@ -2,4 +2,3 @@ import pickle
 async def botlog(subcmd):
     if subcmd=="config":
         message.channel.send("What channel would you like log messages to be posted in?")
-        await on_message()
