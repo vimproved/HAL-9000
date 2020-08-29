9f7a8334e74f6940e8e69a0d2bd15a7ce5e2dbdb
Paradigmmmm <retroturtlerocks@gmail.com>
2020-08-29 18:58:01 -0400

    Added fetch_message()

--- botlog.py
+++ botlog.py
@@ -4,20 +4,18 @@ async def botlog(args, bot, channel, message):
     if args=="logchannel ":
         msgauth1 = message.author
         await channel.send("What channel would you like log messages to be posted in?")
-        while channel.last_message_id.author != message.author:
-            time.sleep(0.1)
-            print(str(channel.last_message_id.author)+" "+str(message.author))
-        botlogchannel = channel.last_message_id.content
+        botlogchannel = await fetch_message(channel.last_message_id)
+        botlogchannel = botlogchannel.id
         guildchannellist = pickle.load(open("guildchannellist", "rb"))
         guildchannellist.update({message.guild.id: botlogchannel})
         pickle.dump(guildchannellist, open("guildchannellist", "wb"))
         await channel.send('Would you like to configure demotion/promotion logging?')
-        answer = channel.last_message.content
+        answer = await fetch_message(channel.last_message_id)
         if answer.tolowercase == "yes" or "y":
             guildrolelist2=[]
             guildrolelist = pickle.load(open("guildrolelist", "rb"))
             await channel.send("Cool! How many ranks do you have?")
-            answer = channel.last_message
+            answer = await fetch_message(channel.last_message_id)
             for x in range (0,int(answer.role.id)+1):
                 await channel.send(channel.last_message)
                 answer = channel.last_message
