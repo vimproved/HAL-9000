67ac7562d19074ffc8dd9a737655acf7f9051784
Paradigmmmm <retroturtlerocks@gmail.com>
2020-08-28 21:28:48 -0400

    woop woop haldoum alert

--- botlog.py
+++ botlog.py
@@ -3,13 +3,13 @@ async def botlog(args, bot, channel, message):
     if args=="logchannel ":
         msgauth1 = message.author
         await channel.send("What channel would you like log messages to be posted in?")
-        botlogchannel = channel.last_message
+        botlogchannel = channel.last_message.content
         guildchannellist = pickle.load(open("guildchannellist", "rb"))
         guildchannellist.update({message.guild.id: botlogchannel})
         pickle.dump(guildchannellist, open("guildchannellist", "wb"))
         await channel.send('Would you like to configure demotion/promotion logging?')
-        answer = channel.last_message
-        if answer.content.tolowercase == "yes" or "y":
+        answer = channel.last_message.content
+        if answer.tolowercase == "yes" or "y":
             guildrolelist2=[]
             guildrolelist = pickle.load(open("guildrolelist", "rb"))
             await channel.send("Cool! How many ranks do you have?")
