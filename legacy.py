5442fac1161fcb0e334b93b1d055cc86cbcdfa85
Paradigmmmm <retroturtlerocks@gmail.com>
2020-08-28 20:19:30 -0400

    Fixed coinflip.

--- legacy.py
+++ legacy.py
@@ -12,6 +12,7 @@ async def ping_msg(args, bot, channel):
 	"""Pings the bot. Ignores arguments."""
 	await channel.send("Pong! :ping_pong:")
 cmds = {"time": ("Displays the time.", display_time),
-	"ping": ("Responds with a pong message.", ping_msg)}
+	"ping": ("Responds with a pong message.", ping_msg)
+	"coinflip": ("Flips a coin."), flip_coin}
 #i am a gnome
 desc = "Old commands from previous iterations of the bot."
