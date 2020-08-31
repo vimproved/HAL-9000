43b8a78149bed24140a544f74efdac36c3302278
Paradigmmmm <retroturtlerocks@gmail.com>
2020-08-31 10:12:49 -0400

    Added //ban

--- mod.py
+++ mod.py
@@ -1,9 +1,12 @@
 async def ban(args, bot, channel, message):
     user = args
     banroleids = [738456842707140700, 742128809129803806, 742128992286670910, 742129191277035590]
+    userbanroles = []
     for x in user.roles:
         if x.id in banroleids:
-            if x != 3:
-                await user.add_roles(banroleids.index(x)+1)
-            else:
-                await chanel.send("That user is already the highest banned level.")
+            userbanroles.append(x)
+    x = userbanroles[-1]
+    if x != 3:
+        await user.add_roles(banroleids.index(x) + 1)
+    else:
+        await channel.send("That user is already the highest banned level.")
