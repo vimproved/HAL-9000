a53669a0ff080e50db224bc7257702781d0bf72c
Paradigmmmm <retroturtlerocks@gmail.com>
2020-08-31 12:54:06 -0400

    Fixed //ban (hopefully)

--- mod.py
+++ mod.py
@@ -1,5 +1,5 @@
-async def ban(args, bot, channel, message):
-    user = args
+async def ban(args : discord.Member, bot, channel, message):
+    await user = args
     banroleids = [738456842707140700, 742128809129803806, 742128992286670910, 742129191277035590]
     userbanroles = []
     for x in user.roles:
