dfb5738f7e0a09fdc498eac6a46c4a558d76349f
Paradigmmmm <retroturtlerocks@gmail.com>
2020-09-03 11:07:06 -0400

    fDFIDJSHF

--- main.py
+++ main.py
@@ -86,8 +86,7 @@ async def ban(ctx, args):
         await user.add_roles(y)
         await ctx.send("Ban role " + str(y) + " successfully added to user " + args)
     else:
-        await ban(ctx, str(user))
-        await ctx.send("User " + args + " hath been yeeted.")
+        await ctx.send("User " + args + " has all the banned roles already.")
 
 
 bot.run(open("token").read())
