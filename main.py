b876513c78e867197ee68a1b83dc4bd7d646cbb1
SoupOfDepression <50428065+SoupOfDepression@users.noreply.github.com>
2020-09-04 11:04:45 -0400

    Fix the fact that this only takes 1 argument normally and I was being clonor
    turns out my multiple banning thing didnt work because discord was only using one argument

--- main.py
+++ main.py
@@ -82,14 +82,14 @@ async def roll(ctx, args):
 
 
 @bot.command()
-async def ban(ctx, args):
+async def ban(ctx, *args):
     """'Bans' a user."""
     if (any([aghbo.permissions.manage_roles for aghbo in ctx.author.roles])):
         converter = MemberConverter()
         converter2 = RoleConverter()
         banroleids = [738456842707140700, 742128809129803806, 742128992286670910, 742129191277035590]
-        for cycl in range(0,len(args.split())):
-            user = await converter.convert(ctx, args.split()[cycl])
+        for cycl in range(0,len(args)):
+            user = await converter.convert(ctx, args[cycl])
             userbanroles = []
             for bancycle in banroleids:
                 for x in user.roles:
@@ -106,14 +106,14 @@ async def ban(ctx, args):
             if x == 0:
                 y = await converter2.convert(ctx, "738456842707140700")
                 await user.add_roles(y)
-                await ctx.send("Ban role " + str(y) + " successfully added to user " + args.split()[cycl])
+                await ctx.send("Ban role " + str(y) + " successfully added to user " + args[cycl])
             elif x != 742129191277035590:
                 y = await converter2.convert(ctx, str(banroleids[banroleids.index(x) + 1]))
                 await user.add_roles(y)
                 [await user.remove_roles(thisshouldntbeplural) for thisshouldntbeplural in ([await converter2.convert(ctx, str(banroleids[z])) for z in range(0,banroleids.index(x)+1)])]
-                await ctx.send("Ban role " + str(y) + " successfully added to user " + args.split()[cycl])
+                await ctx.send("Ban role " + str(y) + " successfully added to user " + args[cycl])
             else:
-                await ctx.send("User " + args.split()[cycl] + " has all the banned roles already.")
+                await ctx.send("User " + args[cycl] + " has all the banned roles already.")
     else:
         await ctx.send("You do not have permission to use this command")
 
