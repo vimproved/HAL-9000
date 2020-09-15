82c8a8b92028fc63037a809357097bbeeb7dca09
Paradigmmmm <retroturtlerocks@gmail.com>
2020-09-15 08:50:44 -0400

    Made //help case insensitive.

--- cogs/util.py
+++ cogs/util.py
@@ -15,6 +15,7 @@ class Utility(commands.Cog):
 
     @commands.command()
     async def help(self, ctx, args=""):
+        args = args.lower()
         if args in self.bot.cogs.keys():
             args = self.bot.get_cog(args)
             embed_var = discord.Embed(color=0xff0008)
