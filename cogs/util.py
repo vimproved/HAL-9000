6b1f6726b33d949f626b0b54a4c2e0a79dd0e988
Paradigmmmm <retroturtlerocks@gmail.com>
2020-09-17 12:20:50 -0400

    Added //bulkdelete.

--- cogs/util.py
+++ cogs/util.py
@@ -187,6 +187,5 @@ class Utility(commands.Cog):
         deletionlist=[]
         async for message in ctx.channel.history(limit=int(args)+1):
             deletionlist.append(message)
-        for yeet in deletionlist:
-            await yeet.delete()
+        await ctx.channel.delete(deletionlist)
         await ctx.send("Deleted " + args + " messages.")
