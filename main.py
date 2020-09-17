3740ea0cb27f0d8c883b1d4b851716fe4aa4b6b7
Paradigmmmm <retroturtlerocks@gmail.com>
2020-09-17 13:09:07 -0400

    wow it works ok

--- main.py
+++ main.py
@@ -87,14 +87,13 @@ class HAL(commands.Bot):
                         afterrole = (after.guild.get_role(guildrolelist(guildrolelist.index(x)-1))).name
                     except Exception:
                         afterrole = "user"
-                    await logchannel.send(after.mention + " has been demoted from *" + x.name + "* to *" + afterrole.name + "*.")
+                    await logchannel.send(after.mention + " has been demoted from *" + x.name + "* to *" + afterrole + "*.")
                 elif len(after.roles) > len(before.roles):
                     try:
                         beforerole = (after.guild.get_role(guildrolelist(guildrolelist.index(x)+1))).name
                     except Exception:
                         beforerole = "user"
-                    await logchannel.send(
-                        await logchannel.send(after.mention + " has been demoted from *" + x.name + "* to *" + beforerole + "*."))
+                        await logchannel.send(after.mention + " has been promoted from *" + beforerole + "* to *" + x.name + "*."))
 
 
 bot = HAL("//")
