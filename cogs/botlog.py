3836023f11469eb858696e093e077ae4175dcb79
Paradigmmmm <retroturtlerocks@gmail.com>
2020-09-17 10:35:27 -0400

    Added new response code to //config

--- cogs/botlog.py
+++ cogs/botlog.py
@@ -19,10 +19,14 @@ class Botlog(commands.Cog):
         """Configures botlogger."""
         rconvert = RoleConverter()
         tcconvert = TextChannelConverter()
-        await ctx.send("What channel would you like log messages to be posted in?")
-        response = await ctx.channel.fetch_message_fast(ctx.channel.last_message_id)
-        while response.author != ctx.author:
-            response = await ctx.channel.fetch_message_fast(ctx.channel.last_message_id)
+        q = await ctx.send("What channel would you like log messages to be posted in?")
+        responsefound = False
+        while not responsefound:
+            async for message in ctx.channel.history(limit=10):
+                if message.author == ctx.author and message.created_at > q.created_at:
+                    response = message
+                    responsefound = True
+                    break
         answer = response.content
         answer = await tcconvert.convert(ctx, answer)
         answer = answer.id
@@ -32,10 +36,14 @@ class Botlog(commands.Cog):
             guildchannellist = {}
         guildchannellist.update({ctx.guild.id: answer})
         pickle.dump(dict(guildchannellist), open("guildchannellist", "wb"))
-        await ctx.send('Would you like to configure demotion/promotion logging?')
-        response = await ctx.channel.fetch_message_fast(ctx.channel.last_message_id)
-        while response.author != ctx.author:
-            response = await ctx.channel.fetch_message_fast(ctx.channel.last_message_id)
+        q = await ctx.send('Would you like to configure demotion/promotion logging?')
+        responsefound = False
+        while not responsefound:
+            async for message in ctx.channel.history(limit=10):
+                if message.author == ctx.author and message.created_at > q.created_at:
+                    response = message
+                    responsefound = True
+                    break
         answer = response.content
         if answer.lower() == "yes" or "y":
             guildrolelist2 = []
@@ -43,16 +51,24 @@ class Botlog(commands.Cog):
                 guildrolelist = pickle.load(open("guildrolelist", "rb"))
             except EOFError:
                 guildrolelist = {}
-            await ctx.send("Cool! How many ranks do you have?")
-            response = await ctx.channel.fetch_message_fast(ctx.channel.last_message_id)
-            while response.author != ctx.author:
-                response = await ctx.channel.fetch_message_fast(ctx.channel.last_message_id)
+            q = await ctx.send("Cool! How many ranks do you have?")
+            responsefound = False
+            while not responsefound:
+                async for message in ctx.channel.history(limit=10):
+                    if message.author == ctx.author and message.created_at > q.created_at:
+                        response = message
+                        responsefound = True
+                        break
             answer = response.content
             for x in range(0, int(answer)):
-                await ctx.send("What is the rank #" + str(x + 1) + " in the hierarchy?")
-                response = await ctx.channel.fetch_message_fast(ctx.channel.last_message_id)
-                while response.author != ctx.author:
-                    response = await ctx.channel.fetch_message_fast(ctx.channel.last_message_id)
+                q = await ctx.send("What is the rank #" + str(x + 1) + " in the hierarchy?")
+                responsefound = False
+                while not responsefound:
+                    async for message in ctx.channel.history(limit=10):
+                        if message.author == ctx.author and message.created_at > q.created_at:
+                            response = message
+                            responsefound = True
+                            break
                 answer = await rconvert.convert(ctx, response.content)
                 answer = answer.id
                 guildrolelist2.append(answer)
