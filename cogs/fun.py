bf89e7fb84dfa604068a4b81a0c4cce05a51f350
Paradigmmmm <retroturtlerocks@gmail.com>
2020-09-22 15:37:29 -0400

    Go to #horny-jail.

--- cogs/fun.py
+++ cogs/fun.py
@@ -160,6 +160,7 @@ class Fun(commands.Cog):
     @commands.command()
     async def horny(self, ctx):
         """BONK! Ignores arguments."""
+        await ctx.send("go to #horny-jail")
         await ctx.send(
             "https://media.discordapp.net/attachments/536731263764267009/752009497681068062/Screen_Shot_2020-04"
             "-28_at_12.png?width=1273&height=684")
