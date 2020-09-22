ac4938dcfe87a8821eff00e151907cf02d78cad5
Paradigmmmm <retroturtlerocks@gmail.com>
2020-09-22 11:03:01 -0400

    Botlogging now completely works with //config.

--- main.py
+++ main.py
@@ -61,7 +61,7 @@ class HAL(commands.Bot):
 
     async def on_member_join(self, member):
         config = globalconfig[member.guild.id]
-        systemchannel = discord.utils.get(member.guild.text_channel, id=config["systemchannel"])
+        systemchannel = discord.utils.get(member.guild.text_channels, id=config["systemchannel"])
         embed_var = discord.Embed(color=0xff0008)
         embed_var.add_field(name="__Ahoy There!__",
                             value=member.mention + "joined the server! Make sure to read #readme!")
