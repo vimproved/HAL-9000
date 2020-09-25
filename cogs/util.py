c5b8cfbc7e40e42dd940ad7091938ffc03976b09
Paradigmmmm <retroturtlerocks@gmail.com>
2020-09-25 19:24:41 -0400

    Added //poll, fully functional.

--- cogs/util.py
+++ cogs/util.py
@@ -1,5 +1,6 @@
 from discord.ext import commands
 import discord
+from discord.ext.commands import TextChannelConverter, EmojiConverter
 
 
 def setup(bot):
@@ -61,3 +62,108 @@ class Utility(commands.Cog):
     async def repo(self, ctx):
         """Sends the link to the GitHub repo for HAL. Ignores arguments."""
         await ctx.send("https://github.com/Paradigmmmm/HAL-9000")
+
+    @commands.has_permissions(administrator=True)
+    @commands.command()
+    async def poll(self, ctx, *args):
+        pollchannel = await TextChannelConverter().convert(ctx, args[0])
+        embed = discord.Embed(color=0xff0008)
+        embed.add_field(name="__Poll Creation__", value='What is the name of your poll?\nType "cancel" at any time to cancel poll creation.', inline=False)
+        q = await ctx.send(embed=embed)
+        responsefound = False
+        while not responsefound:
+            async for message in ctx.channel.history(limit=10):
+                if message.author == ctx.author and message.created_at > q.created_at:
+                    response = message
+                    responsefound = True
+                    break
+        answer = response.content
+        if answer.lower() == "cancel":
+            return
+        title = answer
+        embed = discord.Embed(color=0xff0008)
+        embed.add_field(name="__Poll Creation__",
+                        value='Type a brief description of your poll.\nType "cancel" at any time to cancel poll creation.',
+                        inline=False)
+        q = await ctx.send(embed=embed)
+        responsefound = False
+        while not responsefound:
+            async for message in ctx.channel.history(limit=10):
+                if message.author == ctx.author and message.created_at > q.created_at:
+                    response = message
+                    responsefound = True
+                    break
+        answer = response.content
+        if answer.lower() == "cancel":
+            return
+        description = answer
+        embed = discord.Embed(color=0xff0008)
+        embed.add_field(name="__Poll Creation__",
+                        value='How many voting options would you like?.\nType "cancel" at any time to cancel poll creation.',
+                        inline=False)
+        q = await ctx.send(embed=embed)
+        responsefound = False
+        while not responsefound:
+            async for message in ctx.channel.history(limit=10):
+                if message.author == ctx.author and message.created_at > q.created_at:
+                    response = message
+                    responsefound = True
+                    break
+        answer = response.content
+        numresps = answer
+        if answer.lower() == "cancel":
+            return
+        options = {}
+        for x in range(0, int(numresps)):
+            embed = discord.Embed(color=0xff0008)
+            embed.add_field(name="__Poll Creation__",
+                            value='Type the text for response #' + str(x+1) + '.\nType "cancel" at any time to cancel poll creation.',
+                            inline=False)
+            q = await ctx.send(embed=embed)
+            responsefound = False
+            while not responsefound:
+                async for message in ctx.channel.history(limit=10):
+                    if message.author == ctx.author and message.created_at > q.created_at:
+                        response = message
+                        responsefound = True
+                        break
+            answer = response.content
+            if answer.lower() == "cancel":
+                return
+            text = answer
+            embed = discord.Embed(color=0xff0008)
+            embed.add_field(name="__Poll Creation__",
+                            value='Type the emoji for response #' + str(x+1) + '. MAKE SURE THAT THE EMOJI IS EITHER FROM THIS SERVER, OR A GLOBAL EMOJI.\nType "cancel" at any time to cancel poll creation.',
+                            inline=False)
+            q = await ctx.send(embed=embed)
+            responsefound = False
+            while not responsefound:
+                async for message in ctx.channel.history(limit=10):
+                    if message.author == ctx.author and message.created_at > q.created_at:
+                        response = message
+                        responsefound = True
+                        break
+            answer = response.content
+            emoji = answer
+            options.update({emoji: text})
+        await ctx.send("Sending poll message.")
+        respstr = ""
+        for x in options.keys():
+            respstr = respstr + "\n\n" + x + ": " + options[x]
+        embed = discord.Embed(color=0xff0008)
+        embed.add_field(name="*__POLL: " + title + "__*",
+                        value='Description:\n' + description,
+                        inline=False)
+        embed.add_field(name="*Responses*",
+                        value=respstr,
+                        inline=False)
+        embed.add_field(name="*Created by:*",
+                        value=ctx.author.mention,
+                        inline=False)
+        if args[1] == "everyone":
+            thing = "@everyone"
+        else:
+            thing = args[1]
+        poll = await pollchannel.send(thing, embed=embed)
+        for x in options.keys():
+            await poll.add_reaction(x)
