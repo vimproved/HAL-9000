dcb720fe78c079f109e1bbc28ad0dbb4d9bec9f1
Paradigmmmm <retroturtlerocks@gmail.com>
2020-09-24 22:55:20 -0400

    Added minecraft cog with command //mcprofile

---
+++ cogs/minecraft.py
@@ -0,0 +1,33 @@
+from discord.ext import commands
+import base64
+import requests
+import discord
+import json
+
+
+def setup(bot):
+    bot.add_cog(Minecraft(bot))
+
+
+class Minecraft(commands.Cog):
+    """Commands relating to Minecraft."""
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def mcprofile(self, ctx, args):
+        """Gets a Minecraft profile from IGN.
+        ```//mcprofile <IGN>```"""
+        uuidfromign = requests.get("https://api.mojang.com/users/profiles/minecraft/" + args).json()
+        profile = requests.get("https://sessionserver.mojang.com/session/minecraft/profile/" + uuidfromign["id"]).json()
+        base64textures = profile["properties"][0]["value"]
+        base64_bytes = base64textures.encode('ascii')
+        message_bytes = base64.b64decode(base64_bytes)
+        textures = message_bytes.decode('ascii')
+        textures = json.loads(textures)
+        skinurl = textures["textures"]["SKIN"]["url"]
+        embed_var = discord.Embed(color=0xff0008)
+        embed_var.add_field(name="__Name__", value=profile["name"], inline=False)
+        embed_var.add_field(name="__UUID__", value=profile["id"], inline=False)
+        embed_var.add_field(name="__Skin URL__", value=skinurl, inline=False)
+        await ctx.send(embed=embed_var)
--- main.py
+++ main.py
@@ -9,7 +9,7 @@ class HAL(commands.Bot):
         super().__init__(command_prefix, **options)
         self.description = "A multipurpose bot made by vi#7158 and rous#7120"
         self.token = open("token").read()
-        self.loaded_cogs = ["cogs.mod", "cogs.util", "cogs.fun", "cogs.random"]
+        self.loaded_cogs = ["cogs.mod", "cogs.util", "cogs.fun", "cogs.random", "cogs.minecraft"]
         self.startup()
 
     async def on_command_error(self, ctx, exception):
