0840bd463a8bbe7ce6f28a2baf7ce72c31de331d
Violet Purcell <retroturtlerocks@gmail.com>
2021-03-05 15:38:38 -0500

    HAL is watching you

--- main.py
+++ main.py
@@ -3,9 +3,12 @@ from datetime import datetime
 import discord
 import pickle
 import sys
+import toml
+
 
 class HAL(commands.Bot):
     def __init__(self, command_prefix, **options):
+        config = toml.loads(open("config.toml", "rt").read())
         super().__init__(command_prefix, **options)
         self.description = "A multipurpose bot made by vi#7158."
         self.token = open("token").read()
@@ -19,23 +22,18 @@ class HAL(commands.Bot):
             await ctx.send("You are missing permissions required to run this command.")
             return
         elif str(exception).startswith("Command raised an exception: IndexError:"):
-            await ctx.send("This command requires arguments that you did not specify. Do //help <command> for information on how to use this command.")
-            return
-        try:
-            globalconfig = pickle.load(open("config", "rb"))
-        except EOFError:
-            print("Config file is blank. If you're seeing this your installation of HAL is probably new, or a critical error has occurred.")
-            globalconfig = {}
+	        await ctx.send("This command requires arguments that you did not specify. Do //help <command> for information on how to use this command.")
+        	return
         await ctx.send(str(exception))
-        config = globalconfig[ctx.guild.id]
-        logchannel = self.get_channel(int(config['logchannel']))
+        config = toml.loads(open("config.toml", "rt").read())
+        logchannel = self.get_channel(config['logchannel'])
         await logchannel.send("Error log at " + str(datetime.now()) + ": " + str(exception) + " Type: " + str(type(exception)) + ". Invoke message: " + ctx.message.jump_url)
 
     @staticmethod
     async def on_ready():
         print("HAL-9000")
         print("HAL is ready!")
-        await bot.change_presence(activity=discord.Game(name='//help'))
+        await bot.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name='you...'))
 
     @staticmethod
     async def on_connect():
@@ -64,13 +62,7 @@ class HAL(commands.Bot):
                 print("Failed to load cog. Reason: " + str(e))
 
     async def on_member_ban(self, guild, user):
-        try:
-            globalconfig = pickle.load(open("config", "rb"))
-        except EOFError:
-            print("Config file is blank. If you're seeing this your installation of HAL is probably new, or a critical error has occurred.")
-            globalconfig = {}
-        config = globalconfig[guild.id]
-        systemchannel = self.get_channel(int(config['systemchannel']))
+        systemchannel = self.get_channel(config['systemchannel'])
         ban = await guild.fetch_ban(user)
         reason = ban[0]
         if reason == None:
