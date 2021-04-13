d71f9c529833c4ef7ae8f6beb68b0fdf5ccbbaf4
vi-improved <viipurcell@gmail.com>
2021-04-13 11:45:38 -0400

    added package installation to run.sh

--- run.sh
+++ run.sh
@@ -6,6 +6,10 @@ OUTPUT=$(git pull | grep "Already up to date.")
 if [ -z "$OUTPUT" ]
 then
 kill $BOTPID
+pip3 install pillow
+pip3 install discord
+pip3 install requests
+pip3 install emojis
 python3 main.py &
 BOTPID=$!
 echo "Bot rebooted"
