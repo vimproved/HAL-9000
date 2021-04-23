eb432a1eccb55804930e6ddac49d7df6a39534b0
SoupOfDepression <50428065+SoupOfDepression@users.noreply.github.com>
2021-04-23 21:23:20 +0000

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
+cd cone
+make
+cd ..
 python3 main.py &
 BOTPID=$!
 while true
@@ -5,6 +8,9 @@ do
 OUTPUT=$(git pull | grep "Already up to date.")
 if [ -z "$OUTPUT" ]
 then
+cd cone
+make
+cd ..
 kill $BOTPID
 python3 main.py &
 BOTPID=$!
