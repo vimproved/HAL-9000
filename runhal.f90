db38fee09a40ff4d137c529d7c369142a48fc9ec
SoupOfDepression <50428065+SoupOfDepression@users.noreply.github.com>
2021-05-19 16:04:18 +0000

    Might as well modify runhal.f90
    This simplifies runhal.f90 quite a bit and only uses it for how fortran handles ctrl+c and execute_command_line

--- runhal.f90
+++ runhal.f90
@@ -1,20 +1,4 @@
 program halexit
-	logical :: ex
-	character(16) :: ptk
-	
-	inquire(exist=ex,file=".inpid")
-	if (.not.ex) then
-		call execute_command_line("mkfifo .inpid")
-	end if
 	call execute_command_line('bash runb.sh')
-	
-	call execute_command_line("echo 'c' > .inpid&") !freezes if you dont do this
-	open(9,file='.inpid',action='read')
-	read(9,'(A)') ptk
-
-	call execute_command_line('ps -C "python3" -o pid= > .inpid&') !gets the process id
-	read(9,'(A)') ptk
-	call execute_command_line('kill -9 '//trim(adjustl(ptk))) !kills the programs
-
-	close(9)
+	call execute_command_line("kill -9 $(ps -A -o pid= -o args= | grep 'python3 main.py' | awk '{print $1}') 2> /dev/null") !kills the program
 end program halexit
